9a6943fd741bb6cb7985e63ef98a79e9d369aea0
Renuka Fernando <renukapiyumal@gmail.com>
2018-04-04 16:48:47 +0000

    release build

---
+++ static/js/1.6373385c.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{224:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t(0),a=t.n(r),o=t(14),u=t.n(o),c=t(82);it("renders without crashing",function(){var e=document.createElement("div");u.a.render(a.a.createElement(c.default,null),e),u.a.unmountComponentAtNode(e)})}});
+//# sourceMappingURL=1.6373385c.chunk.js.map
